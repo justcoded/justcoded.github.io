db4e74012561de10ed20cfd015a4760744f1020e
Stanislav Gurin <sgurin@justcoded.com>
2019-07-29 18:47:23 +0300

    Update Logo

--- src/js/modules/vue_filter.js
+++ src/js/modules/vue_filter.js
@@ -44,6 +44,7 @@
       filterButtons: [
         { text: 'WordPress', filter: 'WordPress', isActive: false, isDisabled: false },
         { text: 'PHP', filter: 'PHP', isActive: false, isDisabled: false },
+        { text: 'Gulp', filter: 'Gulp', isActive: false, isDisabled: false },
         { text: 'HTML5/CSS3', filter: 'HTML5/CSS3', isActive: false, isDisabled: false },
         { text: 'JavaScript', filter: 'JavaScript', isActive: false, isDisabled: false },
         { text: 'Vue.js', filter: 'Vue.js', isActive: false, isDisabled: false },
@@ -110,7 +111,7 @@
             src: 'assets/images/web_starter.jpg',
             srcset: 'assets/images/web_starter.jpg, assets/images/web_starter-2x.jpg 2x',
           },
-          tags: ['HTML5/CSS3', 'Node.js'],
+          tags: ['Gulp', 'Node.js'],
           isVisible: true,
         },
         {
